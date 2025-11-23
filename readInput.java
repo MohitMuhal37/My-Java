decdf23ad41eaf7e4138ebb31cf66e48ff5fb900
Mohit Muhal <mohitmuhal15122003@gmail.com>
2025-11-23 20:43:51 +0530

    initial commit

---
+++ readInput.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class readInput {
+    public static void main(String args[]) {
+       Scanner sc = new Scanner(System.in);
+       int a, b, c;
+       System.out.println("Enter Numbers"); 
+        a = sc.nextInt();
+        b = sc.nextInt();
+        c = a + b;
+        System.out.println("Your result is "+ c);
+    }
+}
