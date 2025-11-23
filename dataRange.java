2cc161867549a16d815a8c11b71f689e152f809e
Mohit Muhal <mohitmuhal15122003@gmail.com>
2025-11-23 21:31:24 +0530

    initial commit

---
+++ dataRange.java
@@ -0,0 +1,7 @@
+public class dataRange {
+    public static void main(String[] args) {
+        System.out.println("Check Int Range = " + Integer.MIN_VALUE);
+        System.out.println("Check Int Range = " + Integer.MAX_VALUE);
+        System.out.println("Check Int Range = " + Integer.BYTES);
+    }
+}
