97c1854c64c263ff715e28be03edf7500f4bb906
Mohit Muhal <mohitmuhal15122003@gmail.com>
2025-11-26 22:00:04 +0530

    initial commit

---
+++ javaLiteral.java
@@ -0,0 +1,13 @@
+public class javaLiteral {
+    public static void main(String[] args) {
+        byte b = 10;
+        byte b1 = 0b1010;
+        byte b2 = 012;
+        byte b3 = 0xA;
+
+        System.out.println(b);
+        System.out.println(b1);
+        System.out.println(b2);
+        System.out.println(b3);
+    }
+}
