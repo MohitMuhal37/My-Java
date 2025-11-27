49a5850176a1830826152d7c3cb0d41aa6016750
Mohit Muhal <mohitmuhal15122003@gmail.com>
2025-11-27 22:06:03 +0530

    initial commit

--- printUnicode.java
+++ printUnicode.java
@@ -1,6 +1,6 @@
 public class printUnicode {
     public static void main(String[] args) {
-        for(char c = 0x0900; c <= 0x0970; c++){
+        for(char c = 0x0370; c <= 0x03ff; c++){
             System.out.print(c + " ");
         }
     }
