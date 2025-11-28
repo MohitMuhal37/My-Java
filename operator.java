39505d7b214387476d6576d87a095ab27d1f492b
Mohit Muhal <mohitmuhal15122003@gmail.com>
2025-11-28 15:09:26 +0530

    initial commit

---
+++ operator.java
@@ -0,0 +1,10 @@
+import java.lang.*;
+
+public class operator{
+    public static void main(String[] args){
+        int a = 4;
+        int b = 2;
+        int z = a % b;
+        System.out.println(z);
+    }
+}
