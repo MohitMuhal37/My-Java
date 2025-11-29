6de8cda80e7390188673b1d9b5b2454227736882
Mohit Muhal <mohitmuhal15122003@gmail.com>
2025-11-29 19:04:31 +0530

    initial commit

--- bitwiseOperator.java
+++ bitwiseOperator.java
@@ -7,5 +7,11 @@
          System.out.println(z);
          z = (x << b);
          System.out.println(z);
+         z = (x << 2);
+         System.out.println(z);
+         z = ~x;
+         System.out.println(String.format("%s",Integer.toBinaryString(z)));
+         System.out.println(String.format("%s",Integer.toBinaryString(x)));
+         System.out.println(z);
      }
 }
