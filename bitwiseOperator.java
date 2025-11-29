6a417d28048508718b9e86bff9df071c03d44dc5
Mohit Muhal <mohitmuhal15122003@gmail.com>
2025-11-29 18:56:01 +0530

    initial commit

---
+++ bitwiseOperator.java
@@ -0,0 +1,11 @@
+ public class bitwiseOperator{
+     public static void main(String[] args) {
+         int x = 10;
+         int b = 1;
+         int z;
+         z = (x >> b);
+         System.out.println(z);
+         z = (x << b);
+         System.out.println(z);
+     }
+}
