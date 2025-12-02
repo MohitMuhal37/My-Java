f0d4fcb7d82b6df01926f3b01cb415ae7b846a16
Mohit Muhal <mohitmuhal15122003@gmail.com>
2025-12-02 18:26:46 +0530

    initial commit

---
+++ printing.java
@@ -0,0 +1,8 @@
+class printing{
+    public static void main(String[] args) {
+        int x = 10, y = 20;
+        System.out.println(x + y + " sum");
+        System.out.println("sum "+x + y);
+
+    }
+}
