b465c66c11502f768b25486f573dbb995cb6c55b
Mohit Muhal <mohitmuhal15122003@gmail.com>
2025-12-02 18:07:04 +0530

    initial commit

---
+++ wideNarrow.java
@@ -0,0 +1,15 @@
+public class wideNarrow{
+    public static void main(String[] args) {
+        byte b = 4;
+        short s = 200;
+        int i = 3455;
+        long l = 120;
+        float f = 4.89f;
+        double d = 56.87;
+        char c = 10;
+        boolean bl = true;
+
+        b = (byte)(s);
+        System.out.println("b is = "+b);
+    }
+}
