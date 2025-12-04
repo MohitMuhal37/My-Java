35e3ba6fc80b271d58d773b2e8de3de78e31dd3b
Mohit Muhal <mohitmuhal15122003@gmail.com>
2025-12-04 20:02:19 +0530

    initial commit

--- stringPractice.java
+++ stringPractice.java
@@ -4,6 +4,12 @@ public class stringPractice{
         System.out.println(str);
 
         String str1 = new String("Java");
-        System.out.println(str1);
+
+        char c[] = {'A', 'B', 'C', 'D'};
+        String str2 = new String(c);
+
+        byte b[] = {65, 66, 67, 91};
+        String str3 = new String(b,2,2);
+        System.out.println(str3);
     }
 }
