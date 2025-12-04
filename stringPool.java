95bf0b2e3c5b9633bdb0133a0c0fbbad663e87ec
Mohit Muhal <mohitmuhal15122003@gmail.com>
2025-12-04 20:08:33 +0530

    initial commit

---
+++ stringPool.java
@@ -0,0 +1,15 @@
+public class stringPool{
+    public static void main(String[] args) {
+        String str1 = "Java";
+        String str2 = "Java";
+
+
+        System.out.println(str2.toLowerCase());
+        System.out.println(str1);
+        System.out.println(str2);
+
+        int a = 5;
+        int b = 5;
+        System.out.println(a >= b);
+    }
+}
