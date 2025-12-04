8d23919b9efc65e9c68f929052fb5607d195ac47
Mohit Muhal <mohitmuhal15122003@gmail.com>
2025-12-04 19:39:30 +0530

    stringPractice.java

---
+++ stringPractice.java
@@ -0,0 +1,9 @@
+public class stringPractice{
+    public static void main(String[] args) {
+        String str = "Java Program";
+        System.out.println(str);
+
+        String str1 = new String("Java");
+        System.out.println(str1);
+    }
+}
