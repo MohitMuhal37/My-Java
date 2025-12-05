cc9c5e9a595858d88a969d9821e321c1339ea15f
Mohit Muhal <mohitmuhal15122003@gmail.com>
2025-12-05 21:31:40 +0530

    initial commit

--- stringMethods.java
+++ stringMethods.java
@@ -1,6 +1,6 @@
 public class stringMethods{
     public static void main(String[] args) {
-        String str = "Welcome";
+        String str = "  Welcome";
 
         int l = str.length();
         System.out.println(l);
@@ -11,6 +11,8 @@ public class stringMethods{
         str1 = str.toUpperCase();
         System.out.println(str1);
 
+        str1 = str.trim();
+        System.out.println(str1);
 
     }
 }
