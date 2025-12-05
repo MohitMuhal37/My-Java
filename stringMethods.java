ea803163ba82a4961b4d961c35c5c3a0d206f598
Mohit Muhal <mohitmuhal15122003@gmail.com>
2025-12-05 21:29:58 +0530

    initial commit

---
+++ stringMethods.java
@@ -0,0 +1,12 @@
+public class stringMethods{
+    public static void main(String[] args) {
+        String str = "Welcome";
+
+        int l = str.length();
+        System.out.println(l);
+
+        String str1 = str.toLowerCase();
+        System.out.println(str1);
+
+    }
+}
