59891adb18573a274eea225f6d46cb809dcc1058
Mohit Muhal <mohitmuhal15122003@gmail.com>
2025-12-07 20:49:32 +0530

    initial commit

--- stringSymbols.java
+++ stringSymbols.java
@@ -1,6 +1,7 @@
 public class stringSymbols{
     public static void main(String[] args) {
-        String str1 = "abc";
+        String str1 = "%";
         System.out.println(str1.matches("abc"));
+        System.out.println(str1.matches("[^abc]"));
     }
 }
