c2b65e37f9c2888b9b985fcf58da436e0c18a3c3
Mohit Muhal <mohitmuhal15122003@gmail.com>
2025-12-08 20:41:04 +0530

    initial commit

--- stringSymbols.java
+++ stringSymbols.java
@@ -1,7 +1,15 @@
 public class stringSymbols{
     public static void main(String[] args) {
-        String str1 = "%";
+        String str1 = "a";
         System.out.println(str1.matches("abc"));
         System.out.println(str1.matches("[^abc]"));
+
+        //Meta characters
+        System.out.println(str1.matches("\\w"));
+        System.out.println(str1.matches("\\W"));
+        System.out.println(str1.matches("\\d"));
+        System.out.println(str1.matches("\\D"));
+        System.out.println(str1.matches("\\s"));
+        System.out.println(str1.matches("\\S"));
     }
 }
