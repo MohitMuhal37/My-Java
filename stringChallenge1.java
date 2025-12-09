5c751bb05062c8699812c813f75480e0e2269fe1
Mohit Muhal <mohitmuhal15122003@gmail.com>
2025-12-09 20:37:39 +0530

    initial commit

---
+++ stringChallenge1.java
@@ -0,0 +1,10 @@
+//import java.util.lang.*;
+
+public class stringChallenge1{
+    public static void main(String[] args) {
+        int b = 10101;
+        String str  = "01100AF";
+        System.out.println(str.matches("[01 A-F]*"));
+        
+    }
+}
