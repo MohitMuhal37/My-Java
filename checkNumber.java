59ac9b6728f6d0e0c3e619c1c8ee403038ef0660
Mohit Muhal <mohitmuhal15122003@gmail.com>
2025-12-11 20:52:55 +0530

    initial commit

--- checkNumber.java
+++ checkNumber.java
@@ -2,10 +2,23 @@ import java.util.Scanner;
 public class checkNumber{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter First Number = ");
+        System.out.print("Enter First Number = ");
         int a = sc.nextInt();
-        System.out.println("Enter Second Number = ");
+        System.out.print("Enter Second Number = ");
         int b = sc.nextInt();
+        System.out.print("Enter third Number = ");
+        int c = sc.nextInt();
 
+        if(a > b && a > c){
+            System.out.println("A is The Greater Number");
+        }
+        else{
+            if (b > c){
+                System.out.println("B is Greater than c");
+            }
+            else{
+                System.out.println("C is Greater");
+            }
+        }
     }
 }
