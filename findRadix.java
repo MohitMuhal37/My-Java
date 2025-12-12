8d51f1596dabb304a8bffe9926cffb32431c049b
Mohit Muhal <mohitmuhal15122003@gmail.com>
2025-12-12 12:44:56 +0530

    initial commit

--- findRadix.java
+++ findRadix.java
@@ -1,10 +1,11 @@
 import java.util.Scanner;
 
 class findRadix{
-    public static void main(String args[]){
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter String number here = ");
         String num = sc.nextLine();
+
         if(num.matches("[01]+")){
             System.out.print("Radix is Binary = "+num);
         }
--- first.java
+++ first.java
@@ -1,5 +1,5 @@
 import java.util.*;
- class first{
+public class first{
     public static void main(String args[]){
         System.out.println("Hello World");
     }
