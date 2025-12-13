56b9ea0c95167ad5c601367917579d6945cda332
Mohit Muhal <mohitmuhal15122003@gmail.com>
2025-12-13 17:23:30 +0530

    initial commit

--- demo.java
+++ demo.java
@@ -1,6 +1,11 @@
 public class demo {
-    public static void main(String[] args)
-    {
-        System.out.println("hola");
+    public static void main(String[] args) {
+        String name = "MadaM";
+        if (name.startsWith("Ma") && name.endsWith("aM")) {
+            System.out.println("True");
+        }
+        else{
+            System.out.println("False");
+        }
     }
 }
