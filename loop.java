50fafc2006c8c866e0a1d1967837c79b67fe126f
Mohit Muhal <mohitmuhal15122003@gmail.com>
2025-12-14 18:10:09 +0530

    initial commit

--- loop.java
+++ loop.java
@@ -1,13 +1,13 @@
 class loop{
     public static void main(String[] args) {
         int i = 1;
-        while(i <= 100){
-            System.out.print(i + " ");
-            i = i * 2;
-        }
-
+//        while(i <= 100){
+//            System.out.print(i + " ");
+//            i = i * 2;
+//        }
+        System.out.println();
         do{
-            System.out.println(i + " ");
+            System.out.print(i + " ");
             i *= 2;
         }while (i <= 100);
     }
