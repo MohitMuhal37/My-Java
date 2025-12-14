e3619ec16f579e6870913b769c4b6064c09c6536
Mohit Muhal <mohitmuhal15122003@gmail.com>
2025-12-14 18:45:52 +0530

    initial commit

--- loopPractice.java
+++ loopPractice.java
@@ -4,9 +4,15 @@ class loopPractice{
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter Number Here = ");
         int num = sc.nextInt();
-        for(int i = 1; i <= 10; i++){
-            int val = num * i;
-            System.out.println(val);
+        int sum = 0;
+//        for(int i = 1; i <= 10; i++){
+//            int val = num * i;
+//            System.out.println(val);
+//        }
+
+        for(int i = 0; i <= 10; i++){
+            sum = num + i;
+            System.out.println(sum);
         }
     }
 }
