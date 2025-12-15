e68ecc45b7799a1c75ef9bf6c017b9bd9688de9b
Mohit Muhal <mohitmuhal15122003@gmail.com>
2025-12-15 19:16:28 +0530

    initial commit

---
+++ displayDigitloop.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class displayDigitloop {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Number Here :- ");
+        int num = sc.nextInt();
+        while(num > 0){
+            int r = num % 10;
+            num = num / 10;
+            System.out.print(r+" ");
+        }
+    }
+}
