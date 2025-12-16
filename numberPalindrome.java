339745608d2a9c6cb8458d1bd7f8dc4d65ab707e
Mohit Muhal <mohitmuhal15122003@gmail.com>
2025-12-16 21:03:53 +0530

    initial commit

--- numberPalindrome.java
+++ numberPalindrome.java
@@ -3,16 +3,22 @@ import java.util.Scanner;
 public class numberPalindrome {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter Number here :- ");
+        System.out.print("Enter Number here :- ");
         int num = sc.nextInt();
         int r;
         int rev = 0;
-        int num1 = rev;
+        int num1 = num;
         while(num > 0){
             r = num % 10;
             rev = rev * 10 + r;
             num = num / 10;
         }
-        System.out.println("Your Reverse Number is :- ");
+//        System.out.println("Your Reverse Number is :- "+ rev);
+        if (rev == num1){
+            System.out.print("Number is Palindrome because " +num1 + " = " + rev);
+        }
+        else{
+            System.out.println("Number is Not a Palindrome");
+        }
     }
 }
