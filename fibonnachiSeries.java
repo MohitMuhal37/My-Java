d1c71f337a788903973a51f82e893c47c4193c41
Mohit Muhal <mohitmuhal15122003@gmail.com>
2025-12-17 14:11:36 +0530

    initial commit

---
+++ fibonnachiSeries.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class fibonnachiSeries {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Fibonacchi Length = ");
+        int num = sc.nextInt();
+        int start = 0;
+        int next = 1;
+        int c, i;
+        System.out.print(start + ", " + next + ", ");
+        for(i = 0; i < num - 2; i++){
+            c = start + next;
+            start = next;
+            next = c;
+            System.out.print(c + ", ");
+        }
+    }
+}
