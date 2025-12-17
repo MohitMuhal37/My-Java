7b7b6e5b0a0638f1836367c0b7cbea8e685f2263
Mohit Muhal <mohitmuhal15122003@gmail.com>
2025-12-17 20:56:54 +0530

    initial commit

--- linearSearch.java
+++ linearSearch.java
@@ -1,10 +1,10 @@
 import java.util.Scanner;
-
 public class linearSearch {
     public static void main(String[] args) {
-        int arr[] ={12, 23, 45, 65, 67, 76, 88};
         Scanner sc = new Scanner(System.in);
-        int key = 76;
+        int arr[] ={12, 23, 45, 65, 67, 76, 88};
+        System.out.print("Enter Key you want to find :- ");
+        int key = sc.nextInt();
         for(int i = 0; i < arr.length; i++){
             if(arr[i] == key){
                 System.out.print("Key is found At location of :- "+i);
