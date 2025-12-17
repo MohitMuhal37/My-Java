812eb98cdb0dec649334245689b452a6af3508b8
Mohit Muhal <mohitmuhal15122003@gmail.com>
2025-12-17 13:49:22 +0530

    initial commit

--- apSeries.java
+++ apSeries.java
@@ -3,16 +3,20 @@ import java.util.Scanner;
 public class apSeries {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int i;
+        int i = 0;
         System.out.print("Enter First Number = ");
         int fnum = sc.nextInt();
         System.out.print("Enter Difference = ");
         int diff = sc.nextInt();
         System.out.print("Enter length of Number where You want to print this series = ");
         int len = sc.nextInt();
-        for(i = 0; i < len; i++){
-            System.out.print(fnum+", ");
-           fnum = fnum + diff;
+//        for(i = 0; i < len; i++){
+//            System.out.print(fnum+", ");
+//           fnum = fnum + diff;
+            while(i < len){
+                System.out.print(fnum+", ");
+                fnum = fnum + diff;
+                i++;
         }
     }
 }
