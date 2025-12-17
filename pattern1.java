dd39de5ae1f731fd36f1f82dbd33cc6664361a10
Mohit Muhal <mohitmuhal15122003@gmail.com>
2025-12-17 17:12:54 +0530

    initial commit

--- pattern1.java
+++ pattern1.java
@@ -20,9 +20,16 @@ public class pattern1 {
 //            }
 //            System.out.println();
 //        }
+//        for(int i = 1; i<= 5; i++){
+//            for(int j = 1; j <= i; j++){
+//                System.out.print(++count+"  ");
+//            }
+//            System.out.println();
+//        }
+
         for(int i = 1; i<= 5; i++){
-            for(int j = 1; j <= 5; j++){
-                System.out.format("%02d ",++count);
+            for(int j = 1; j <= 5-i+1; j++){
+                System.out.print(" &");
             }
             System.out.println();
         }
