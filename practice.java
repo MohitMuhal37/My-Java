1f9b16007117918fcea1965a6cfd9297461c26a2
Mohit Muhal <mohitmuhal15122003@gmail.com>
2025-12-18 20:56:53 +0530

    initial commit

--- practice.java
+++ practice.java
@@ -4,13 +4,17 @@ public class practice {
     public static void main(String[] args) {
       // find sum
         int arr[] = {12, 32, 443, 54, 5, 45, 6, 90, 67};
-        int key = 45;
+        int max = arr[0];
+        int max1 = arr[0];
         for(int i = 0; i < arr.length; i++){
-            if (arr[i] == key){
-                System.out.print(i);
-                System.exit(0);
+            if (arr[i] > max){
+                max1 = max;
+                max = arr[i];
+            }
+            else if(arr[i] > max1) {
+                max1 = arr[i];
             }
         }
-            System.out.println("not valid");
+        System.out.println(max1);
     }
 }
