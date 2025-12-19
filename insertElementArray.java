77fee699fcc150146dd50915d487bc2093ff1da5
Mohit Muhal <mohitmuhal15122003@gmail.com>
2025-12-19 20:28:26 +0530

    initial commit

--- insertElementArray.java
+++ insertElementArray.java
@@ -1,18 +1,19 @@
 public class insertElementArray {
     public static void main(String[] args) {
-        int arr[] = new int[8];
-        int n = 5;
+        int arr[] = new int[10];
+        int n = 6;
         int val = 18;
-        int index = 1;
+        int index = 2;
         arr[0] = 11;
         arr[1] = 12;
         arr[2] = 15;
         arr[3] = 23;
         arr[4] = 1;
+        arr[5] = 13;
         for (int i = 0; i < n; i++){
             System.out.print(arr[i]+" ");
-            System.out.println("");
         }
+        System.out.println();
         for(int i = n; i > index; i--){
             arr[i] = arr[i-1];
         }
