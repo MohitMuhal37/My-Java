041498bfbdddd9f79e511b0d318d13ab7e220eec
Mohit Muhal <mohitmuhal15122003@gmail.com>
2025-12-20 19:25:32 +0530

    initial commit

--- practice.java
+++ practice.java
@@ -2,19 +2,14 @@ import java.util.Scanner;
 
 public class practice {
     public static void main(String[] args) {
-      // find sum
-        int arr[] = {12, 32, 443, 54, 5, 45, 6, 90, 67};
-        int max = arr[0];
-        int max1 = arr[0];
-        for(int i = 0; i < arr.length; i++){
-            if (arr[i] > max){
-                max1 = max;
-                max = arr[i];
-            }
-            else if(arr[i] > max1) {
-                max1 = arr[i];
-            }
-        }
-        System.out.println(max1);
+      int arr[] = {12, 34, 54, 6,5 ,76};
+      int temp = arr[0];
+      for(int i = 1; i < arr.length; i++){
+           arr[i-1] = arr[i];
+      }
+      arr[arr.length-1] = temp;
+      for(int x : arr){
+          System.out.print(x+ " ");
+      }
     }
 }
