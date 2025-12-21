c1b5817a7b0fa04e4243d690cee8043f7e325767
Mohit Muhal <mohitmuhal15122003@gmail.com>
2025-12-21 15:41:26 +0530

    initial commit

--- varArguments.java
+++ varArguments.java
@@ -1,6 +1,13 @@
 public class varArguments {
 
+    static void varargs(int ...a){
+        for(int x : a){
+            System.out.print(x+" ");
+        }
+    }
     public static void main (String args[]){
-
+       varargs();
+        varargs(12, 43, 34, 56);
+        varargs(43, 45);
     }
 }
