dd5022ee045a02e2b46dca9d8c948c884e21b193
Mohit Muhal <mohitmuhal15122003@gmail.com>
2025-12-21 13:37:57 +0530

    initial commit

--- reverseNumberOverload.java
+++ reverseNumberOverload.java
@@ -1,5 +1,15 @@
 public class reverseNumberOverload {
-    public static void main(String[] args) {
 
+    static int reverse(int n){
+        int rev = 0;
+        while(n > 0){
+            int r = n % 10;
+            n = n / 10;
+            rev = rev * 10 + r;
+        }
+        return rev;
+    }
+    public static void main(String[] args) {
+        System.out.print(reverse(234));
     }
 }
