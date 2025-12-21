31c3748f5554b0a697e9b6573aa76f144105ba4e
Mohit Muhal <mohitmuhal15122003@gmail.com>
2025-12-21 13:25:00 +0530

    initial commit

---
+++ methodOverloading.java
@@ -0,0 +1,18 @@
+public class methodOverloading {
+    static int max(int a, int b){
+        if (a > b)
+            return a;
+        else
+            return b;
+    }
+
+    static float max(float a, float b){
+        if (a > b)
+            return a;
+        else
+            return b;
+    }
+    public static void main(String[] args){
+        System.out.print(max(12f, 54f));
+    }
+}
