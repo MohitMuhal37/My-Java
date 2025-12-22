986b11edbf1b52329a763377f4d37f71b050467f
Mohit Muhal <mohitmuhal15122003@gmail.com>
2025-12-22 18:33:28 +0530

    initial commit

---
+++ employeClass.java
@@ -0,0 +1,21 @@
+
+class Employee{
+    int id;
+    int age;
+    String name;
+    String lastName;
+    char gender;
+    String branch;
+    String companyMail;
+    double income;
+
+   public void getInput(){
+
+    }
+}
+
+public class employeClass {
+    public static void main(){
+
+    }
+}
