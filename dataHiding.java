ad2f4f7e881c20af64f90173f9756e35f5be1412
Mohit Muhal <mohitmuhal15122003@gmail.com>
2025-12-22 19:40:42 +0530

    initial commit

---
+++ dataHiding.java
@@ -0,0 +1,36 @@
+
+class Employees{
+    private String name;
+    private String lastName;
+    private int id;
+    private int age;
+    private char gender;
+    private String branch;
+    private String companyMail;
+    private double income;
+
+    public void setData(String fName, String lName, int nId, int nAge, char nGender, String nBranch
+    , String nCompanymail, double nIncome){
+        name = fName;
+        lastName = lName;
+        id = nId;
+        age = nAge;
+        gender = nGender;
+        branch = nBranch;
+        companyMail = nCompanymail;
+        income = nIncome;
+    }
+
+    public String toString(){
+        return " Employee Name is = "+name+"\n Last Name = "+lastName+"\n Id is = "+id+
+                "\n Age = "+age+"\n Gender is = "+ gender+ "\n Branch is = "+branch+
+                "\n CompanyMail = "+companyMail+ "\n Income is = "+income;
+    }
+}
+
+public class dataHiding {
+    public static void main(String[] args){
+
+    }
+
+}
