20ec6424aa07fbf9079c1b16788e76faa71fe47d
Mohit Muhal <mohitmuhal15122003@gmail.com>
2025-12-22 18:51:28 +0530

    initial commit

--- employeClass.java
+++ employeClass.java
@@ -9,13 +9,24 @@ class Employee{
     String companyMail;
     double income;
 
-   public void getInput(){
-
+   public void showDetails(){
+       System.out.print("Employee id is = "+id+", Age = "+age+", Name is = "+name+
+               ", LastName = "+lastName+", Gender is = "+ gender+ ", Branch is = "+branch+
+               ", CompanyMail = "+companyMail+ ", Income is = "+income);
     }
 }
 
 public class employeClass {
-    public static void main(){
-
+    public static void main(String[] args){
+    Employee emp = new Employee();
+    emp.id = 101;
+    emp.age = 27;
+    emp.name = "Max";
+    emp.lastName = "Murdock";
+    emp.gender = 'M';
+    emp.branch = "Webwork";
+    emp.companyMail = "max@hotmail.com";
+    emp.income = 55000.65;
+    emp.showDetails();
     }
 }
