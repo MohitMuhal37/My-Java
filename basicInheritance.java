d110e25e6ec711d2afecded9e90a82be13f3eb87
Mohit Muhal <mohitmuhal15122003@gmail.com>
2025-12-23 19:41:10 +0530

    initial commit

---
+++ basicInheritance.java
@@ -0,0 +1,17 @@
+class Circle2{
+    public double radius;
+
+    public double area(){
+        return Math.PI * radius * radius;
+    }
+
+    public double parameter(){
+     return 2 * Math.PI * radius;
+    }
+}
+
+public class basicInheritance {
+    public static void main(String[] args) {
+
+    }
+}
