6580075c6a5886ca0a5005ac62169fa183e071c8
Mohit Muhal <mohitmuhal15122003@gmail.com>
2025-12-24 13:14:42 +0530

    initial commit

---
+++ practiceOverriding1.java
@@ -0,0 +1,25 @@
+class Tv{
+    public void changechannel(){
+        System.out.println("Channel is Changed in TV");
+    }
+
+    public void switchon(){
+        System.out.println("TV is Switchhed on");
+    }
+}
+
+class Smarttv extends Tv{
+    public void changechannel(){
+        System.out.println("In SmartTv Channel is Changed");
+    }
+
+    public void switchon(){
+        System.out.println("Smarttv is Switchhed on");
+    }
+}
+
+public class practiceOverriding1 {
+    public static void main(String[] args) {
+
+    }
+}
