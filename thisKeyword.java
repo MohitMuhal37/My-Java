f298882f0f56f551771a450799e4e9a6ea3ff7ee
Mohit Muhal <mohitmuhal15122003@gmail.com>
2025-12-24 12:19:43 +0530

    initial commit

---
+++ thisKeyword.java
@@ -0,0 +1,20 @@
+class Rectangle1{
+    int length;
+    int breadth;
+    public Rectangle1(int l, int b){
+        length = l;
+        breadth = b;
+    }
+
+    public void display(){
+        System.out.println("Length = "+length);
+        System.out.println("breadth = "+breadth);
+    }
+}
+
+public class thisKeyword {
+    public static void main(String[] args) {
+        Rectangle1 r1 = new Rectangle1(10, 4);
+        r1.display();
+    }
+}
