9987cde049f7e6a94f5e3d0a26078b593cb68eb3
Mohit Muhal <mohitmuhal15122003@gmail.com>
2025-12-24 12:57:21 +0530

    initial commit

---
+++ overriding.java
@@ -0,0 +1,23 @@
+class Super{
+    public void display(){
+        System.out.println("Hello");
+    }
+}
+
+class sub extends Super{
+    public void display(){
+        System.out.println("Hello again");
+    }
+}
+public class overriding {
+    public static void main(String[] args) {
+        Super su = new Super();
+        su.display();
+
+        sub sb = new sub();
+        sb.display();
+
+        Super su1 = new Super();
+        su.display();
+    }
+}
