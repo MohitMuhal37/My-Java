d275715ab2933926689599c5041804f47a06ca32
Mohit Muhal <mohitmuhal15122003@gmail.com>
2025-12-25 18:58:56 +0530

    initial commit

--- abstractExample1.java
+++ abstractExample1.java
@@ -1,3 +1,4 @@
+
 abstract class Shape{
     abstract double area();
     abstract double perimeter();
@@ -18,9 +19,26 @@ class circle2 extends Shape{
         return p;
     }
 }
+class rectangle extends Shape{
+   double length;
+   double breadth;
+   rectangle(double l, double b){
+       this.length = l;
+       this.breadth = b;
+   }
 
+   public double area(){
+       return  length * breadth;
+    }
+
+    public double perimeter(){
+       return  2 * (length + breadth);
+    }
+}
 public class abstractExample1 {
     public static void main(String[] args) {
-
+    Shape s1 = new rectangle(12.98, 34.65);
+        System.out.println("Rectangle Area is :- "+s1.area());
+        System.out.println("Rectangle Perimeter is :- "+s1.perimeter());
     }
 }
