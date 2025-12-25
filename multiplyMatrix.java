e5f111930757a22e8580301b9d71426adb580f53
Mohit Muhal <mohitmuhal15122003@gmail.com>
2025-12-25 18:04:57 +0530

    initial commit

--- multiplyMatrix.java
+++ multiplyMatrix.java
@@ -1,9 +1,9 @@
 public class multiplyMatrix {
     public static void main(String[] args) {
-        int arr[][] = {{12, 3, 5,},
+        int arr[][] = {{1, 3, 5,},
                 {3, 5, 6},
                 {5, 6, 7}};
-        int arr1[][] = {{12, 3, 5,},
+        int arr1[][] = {{1, 3, 5,},
                 {3, 5, 6},
                 {5, 6, 7}};
         int arr2[][] = new int[3][3];
@@ -13,11 +13,18 @@ public class multiplyMatrix {
                 arr2[i][j] = arr[i][j] * arr1[i][j];
             }
         }
-        for (int x[] : arr2) {
-            for (int y : x) {
-                System.out.print(y + " ");
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.printf("%02d ",arr2[i][j]);
             }
             System.out.println();
         }
+//        for (int x[] : arr2) {
+//            for (int y : x) {
+//                System.out.format("%02$d"+y);
+//            }
+//            System.out.println();
+        }
     }
-}
+
