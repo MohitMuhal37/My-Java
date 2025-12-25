5b674c96c88a6d924f0bac7acbf042532e71ff50
Mohit Muhal <mohitmuhal15122003@gmail.com>
2025-12-25 19:53:16 +0530

    initial commit

--- interfaces.java
+++ interfaces.java
@@ -7,7 +7,7 @@ interface t3{
     void meth3();
 }
 
-class te2 implements t1{
+class te2 implements t1, t3{
     public void meth1(){
         System.out.println("Meth 1 is called");
     }
@@ -15,11 +15,17 @@ class te2 implements t1{
     public void meth2(){
         System.out.print("Meth 2 is called");
     }
+
+    public  void meth3(){
+        System.out.println("Meth 3 is called");
+    }
 }
 
 public class interfaces {
     public static void main(String[] args) {
         t1 t = new te2();
+        t3 tt = new te2();
+        tt.meth3();
         t.meth1();
         t.meth2();
     }
