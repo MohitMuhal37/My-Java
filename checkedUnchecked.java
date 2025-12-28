09c530ad2a67d1850633706219a5dd8e313944fe
Mohit Muhal <mohitmuhal15122003@gmail.com>
2025-12-28 17:59:50 +0530

    initail commit

---
+++ checkedUnchecked.java
@@ -0,0 +1,24 @@
+import java.lang.Exception;
+
+public class checkedUnchecked {
+
+    static void fun1(){
+        try {
+            System.out.println(10 / 0);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+    static void fun2(){
+        fun1();
+    }
+    static void fun3(){
+        fun2();
+    }
+
+    public static void main(String[] args)
+    {
+        fun3();
+    }
+}
