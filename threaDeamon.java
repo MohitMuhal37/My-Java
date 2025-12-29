13bdd3eeb5f495b6de095b054479ec4b2b936477
Mohit Muhal <mohitmuhal15122003@gmail.com>
2025-12-29 19:44:51 +0530

    initial commit

--- threaDeamon.java
+++ threaDeamon.java
@@ -11,6 +11,13 @@ class mThread extends Thread{
 public class threaDeamon {
     public static void main(String[] args){
     mThread m = new mThread();
+    m.setDaemon(true);
     m.start();
+    try{
+        Thread.sleep(1000);
+    }
+    catch (InterruptedException e){
+        System.out.println(e);
+    }
     }
 }
