191c6464896c89d7bc50a0b0985ac5abd6a21b62
Mohit Muhal <mohitmuhal15122003@gmail.com>
2025-12-29 21:44:37 +0530

    initial commit

--- synchronisedThread.java
+++ synchronisedThread.java
@@ -1,11 +1,13 @@
 class Mydata
 {
-    public void display(String str)
+ public void display(String str)
     {
+        synchronized(this){
         for(int i = 0; i < str.length(); i++){
             System.out.print(str.charAt(i));
         }
     }
+}
 }
 
 class mythread1 extends Thread
