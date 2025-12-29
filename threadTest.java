9a3cfa90291b210a3dd4c353729cbce103689aac
Mohit Muhal <mohitmuhal15122003@gmail.com>
2025-12-29 19:23:09 +0530

    initial commit

--- threadTest.java
+++ threadTest.java
@@ -1,10 +1,15 @@
-class Myrun implements Runnable
-{
-    public void run(){}
+class myThread extends Thread{
+    public myThread(String name){
+        super(name);
+    }
 }
-
 public class threadTest {
     public static void main(String[] args) {
-        Thread t = new Thread(new Myrun(),"My Name");
+        myThread t = new myThread("My Name");
+        System.out.println("Name "+t.getName());
+        System.out.println("Alive "+t.isAlive());
+        System.out.println("Id "+t.threadId());
+        System.out.println("Priority "+t.getPriority());
+        System.out.println("State "+t.getState());
     }
 }
