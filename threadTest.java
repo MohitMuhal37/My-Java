22fc91663308757845f5661bfc9dbcafbb9caf92
Mohit Muhal <mohitmuhal15122003@gmail.com>
2025-12-29 19:26:37 +0530

    initial commit

--- threadTest.java
+++ threadTest.java
@@ -1,15 +1,19 @@
 class myThread extends Thread{
-    public myThread(String name){
+    public myThread(String name)
+    {
         super(name);
+        setPriority(Thread.MAX_PRIORITY);
     }
 }
 public class threadTest {
     public static void main(String[] args) {
         myThread t = new myThread("My Name");
+        System.out.println(t.getId());
         System.out.println("Name "+t.getName());
         System.out.println("Alive "+t.isAlive());
         System.out.println("Id "+t.threadId());
         System.out.println("Priority "+t.getPriority());
+        t.start();
         System.out.println("State "+t.getState());
     }
 }
