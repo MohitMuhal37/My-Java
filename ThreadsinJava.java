374457b6c5db3ab8821445606d093a0a29f301bf
Mohit Muhal <mohitmuhal15122003@gmail.com>
2025-12-29 17:25:54 +0530

    initial commit

---
+++ ThreadsinJava.java
@@ -0,0 +1,22 @@
+// class MyThread extends Thread{
+//     public void run()
+//     {
+//         int i = 1;
+//         while(true){
+//             System.out.println(i+"Hello");
+//             i++;
+//         }
+//     }
+// }
+
+public class ThreadsinJava extends Thread{
+    public static void main(String[] args) {
+        MyThread mt = new MyThread();
+        mt.start();
+        int i = 1;
+        while(true){
+            System.out.println(i+"World");
+            i++;
+        }
+    }
+}
