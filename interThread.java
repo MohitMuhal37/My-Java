722b4ef7a80798ecc599e6c58b68b911909acdc6
Mohit Muhal <mohitmuhal15122003@gmail.com>
2025-12-30 17:23:40 +0530

    initial commit

--- interThread.java
+++ interThread.java
@@ -1,56 +1,83 @@
-class Mydata{
+class Mydata
+{
     int value = 0;
     boolean flag = true;
-
-    public void set(int v){
-        while (flag != true) {
-            
+    public void set(int v)
+    {
+        while(flag != true){
+            try{
+                wait();
+            }catch(Exception e){}
+        value = v;
+        flag = false;
+        notify();
         }
     }
 
-    public int get(){
+    public int get()
+    {
         int x = 0;
-        x = value;
-        return x; 
+        while(flag != false){
+          try{
+            wait();
+          }catch(Exception e){}
+            x = value;
+        flag = true;
+        notify();
+        }
+        return x;
     }
 }
 
 class Producer extends Thread
 {
-    Mydata d;
-    Producer(Mydata dat)
+    Mydata data;
+
+    public Producer(Mydata d)
     {
-        d = dat;
+        data = d;
     }
 
     public void run()
     {
-        int  i = 1;
-        while (true) {
-            d.set(i);
-            System.out.print("Producer = "+i);
-            i++;
+        int count = 1;
+        while(true){
+           data.set(count);
+           System.out.println("Producer = "+count); 
+           count++;
         }
     }
 }
 
 class Consumer extends Thread
 {
-    Mydata d;
-    Consumer(Mydata dat)
+    Mydata data;
+
+    public Consumer(Mydata d)
     {
-        d = dat;
+        data = d;
     }
 
     public void run()
     {
-        while (true) {
-            System.out.print("Producer = "+d.get());
+        while(true){
+           int value;
+           value = data.get();
+           System.out.println("Producer = "+value); 
         }
     }
 }
-public class interThread {
-   public static void main(String[] args) {
-    
-   } 
+
+
+
+public class interThread
+{
+    public static void main(String[] args) 
+    {
+        Mydata data = new Mydata();
+        Producer p = new Producer(data);
+        Consumer c = new Consumer(data);
+        p.start();
+        c.start();
+    }
 }
