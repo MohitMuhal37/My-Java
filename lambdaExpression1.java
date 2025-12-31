0f778a1313600d3b34a168a264945c281e7d036c
Mohit Muhal <mohitmuhal15122003@gmail.com>
2025-12-31 18:43:34 +0530

    initial commit

---
+++ lambdaExpression1.java
@@ -0,0 +1,17 @@
+interface myLambda
+{
+    public void meth1();
+}
+
+class demo
+{
+   myLambda l = ()->{System.out.println("Hello");
+    System.out.println("Bye");
+   };
+}
+
+public class lambdaExpression1 {
+    public static void main(String[] args) {
+        
+    }
+}
