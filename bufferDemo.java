69c58ed9f8a57a477a732dd1bdf76b7c3f5cf142
Mohit Muhal <mohitmuhal15122003@gmail.com>
2026-01-01 19:21:57 +0530

    initial commit

--- bufferDemo.java
+++ bufferDemo.java
@@ -1,6 +1,8 @@
 import java.io.BufferedInputStream;
+import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 
 public class bufferDemo {
@@ -8,10 +10,19 @@ public class bufferDemo {
         try{
     FileInputStream fis = new FileInputStream("src1.txt");
     BufferedInputStream bis = new BufferedInputStream(fis);
+    FileReader fr = new FileReader("src1.txt");
+    BufferedReader br = new BufferedReader(fr);
             int x;
     while ((x = bis.read())!= -1) {
         System.out.print((char)x);
     }
+    bis.mark(10);
+     System.out.print((char)br.read());
+     System.out.print((char)br.read());
+     System.out.print((char)br.read());
+     System.out.print((char)br.read());
+     bis.reset();
+     System.out.print((char)br.read());
     System.out.println();
     System.out.println(bis.markSupported());
     System.out.println(fis.markSupported());
