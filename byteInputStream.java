e00abd7978ed1dc9225613d76d074c53ac138d28
Mohit Muhal <mohitmuhal15122003@gmail.com>
2026-01-01 18:36:25 +0530

    initial commit

--- byteInputStream.java
+++ byteInputStream.java
@@ -4,10 +4,13 @@ public class byteInputStream {
     public static void main(String[] args) {
         byte b[] = {'a', 'b', 'c', 'd', 'e', 'f'};
         ByteArrayInputStream bis  = new ByteArrayInputStream(b);
-        int x;
-        while((x = bis.read())!= -1){
-            System.out.print((char)x);
-        }
+        // int x;
+        // while((x = bis.read())!= -1){
+        //     System.out.print((char)x);
+        // }
+
+        String str = new String(bis.readAllBytes());
+        System.out.print(str);
         try{
         bis.close();
         }catch(IOException e){
