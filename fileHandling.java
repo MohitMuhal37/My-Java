63151b42a7032801471064c428b9e3722e749f19
Mohit Muhal <mohitmuhal15122003@gmail.com>
2026-01-01 20:54:53 +0530

    initial commit

--- fileHandling.java
+++ fileHandling.java
@@ -1,5 +1,14 @@
+import java.io.File;
 public class fileHandling {
     public static void main(String[] args) {
-        
+      File f = new File("c:/Users/HP/OneDrive/Desktop/Demo");
+      System.out.println(f.canExecute());
+      String str[] = f.list();
+      File str1[] = f.listFiles();
+
+      for(File x : str1){
+        System.out.print(x.getName());
+        System.out.println(x.getPath());
+      }
     }
 }
