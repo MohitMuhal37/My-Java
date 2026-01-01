8de9c1f5306c4828ee29015833fc04c4adaffb90
Mohit Muhal <mohitmuhal15122003@gmail.com>
2026-01-01 18:42:29 +0530

    initial commit

--- byteInputStream.java
+++ byteInputStream.java
@@ -1,9 +1,19 @@
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 public class byteInputStream {
     public static void main(String[] args) {
         byte b[] = {'a', 'b', 'c', 'd', 'e', 'f'};
         ByteArrayInputStream bis  = new ByteArrayInputStream(b);
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        bos.write('a');
+        bos.write('b');
+
+        byte b1[] = bos.toByteArray();
+        for(byte x : b1){
+            System.out.println((char) x);
+        }
+
         // int x;
         // while((x = bis.read())!= -1){
         //     System.out.print((char)x);
