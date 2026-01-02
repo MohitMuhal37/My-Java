7499be7adb55fdc1d4e101167d9f2e96c12e9756
Mohit Muhal <mohitmuhal15122003@gmail.com>
2026-01-02 17:45:42 +0530

    initial commit

--- genericClass.java
+++ genericClass.java
@@ -8,7 +8,7 @@ class Data<T>{
         return obj;
     }
 }
-
+@SuppressWarnings("uncheked")
 class MyArray<T>{
 T A[] = (T[]) new Object[10];
 int length = 0;
@@ -17,8 +17,8 @@ public void append(T v){
     A[length++] = v;
 }
 public void display(){
-    for(T x : A){
-        System.out.println(x);
+    for(int i = 0; i < A.length; i++){
+        System.out.println(A[i]);
     }
 }
 }
@@ -29,5 +29,13 @@ public class genericClass
         Data<Integer> i = new Data();
         i.setData(10);
         System.out.println(i.getData());
+
+        MyArray<Integer> it = new MyArray<>();
+        it.append(12);
+        it.append(13);
+        it.append(11);
+        it.append(2);
+
+        it.display();
     }
 }
