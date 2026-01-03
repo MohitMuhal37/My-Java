79351bfe4799126dcee760de18f761b99dc5931e
Mohit Muhal <mohitmuhal15122003@gmail.com>
2026-01-03 20:51:49 +0530

    initial commit

--- treeMap.java
+++ treeMap.java
@@ -5,6 +5,7 @@ public class treeMap {
     public static void main(String[] args) {
         TreeMap<Integer,String> tm = new TreeMap<>(Map.of(0,"Mohit",1,"Mat"));
         System.out.println(tm.get(1));
+        tm.put(2, "Rohan");
         System.out.println(tm.containsKey(1));
         
     }
