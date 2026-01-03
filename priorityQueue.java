de2607f3af73c5c27e97370e74984437796bcf39
Mohit Muhal <mohitmuhal15122003@gmail.com>
2026-01-03 17:25:30 +0530

    initial commit

--- priorityQueue.java
+++ priorityQueue.java
@@ -1,9 +1,20 @@
+import java.util.Comparator;
 import java.util.Iterator;
 import java.util.PriorityQueue;
 
+class Mycom implements Comparator<Integer>{
+    public int compare(Integer o1, Integer o2){
+        if(o1 < o2)
+            return 1;
+        if(o1 > o2) 
+            return -1;
+        return 0;
+    }
+}
+
 public class priorityQueue {
     public static void main(String[] args) {
-       PriorityQueue<Integer> p = new PriorityQueue<>();
+       PriorityQueue<Integer> p = new PriorityQueue<>(new Mycom());
        p.offer(123);
        p.offer(8); 
        p.offer(18); 
