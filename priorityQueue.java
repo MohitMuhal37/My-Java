18c60acb65d531b1ab4e9a82d06738a5e9cbc668
Mohit Muhal <mohitmuhal15122003@gmail.com>
2026-01-03 17:13:42 +0530

    initial commit

---
+++ priorityQueue.java
@@ -0,0 +1,19 @@
+import java.util.Iterator;
+import java.util.PriorityQueue;
+
+public class priorityQueue {
+    public static void main(String[] args) {
+       PriorityQueue<Integer> p = new PriorityQueue<>();
+       p.offer(123);
+       p.offer(8); 
+       p.offer(18); 
+       p.offer(82); 
+       p.offer(38); 
+       p.offer(68); 
+
+       Iterator<Integer> pq = p.iterator();
+       while(pq.hasNext()){
+        System.out.print(pq.next()+" ");
+       }
+    }
+}
