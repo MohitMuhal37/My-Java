3d545bde421af9a70f2b99eef72e3e06bba89fb7
Mohit Muhal <mohitmuhal15122003@gmail.com>
2026-01-04 12:00:37 +0530

    initial commit

--- stringTokenizer.java
+++ stringTokenizer.java
@@ -1,12 +1,21 @@
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.StringTokenizer;
 
 public class stringTokenizer {
     public static void main(String[] args) {
-        String data = "name=Mohit;adress=Krishanpura;countary=India;dept=cse";
-        StringTokenizer st = new StringTokenizer(data,"=;");
+       try{
+        FileInputStream fis = new FileInputStream("src4.txt");
+        byte b[] = new byte[fis.available()];
+        fis.read();
+        String data = new String(b);
+        // String data = "name=Mohit;adress=Krishanpura;countary=India;dept=cse";
+        StringTokenizer st = new StringTokenizer(data,"=");
         String s;
         while (st.hasMoreTokens()) {
-            System.out.println(st.nextToken());
+            s = st.nextToken();
+            System.out.println(s);
         }
+    }catch(IOException e){}
     }
 }
