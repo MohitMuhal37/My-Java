2848894f0aa2a769eaf44efbb2df44d5c29f36c4
Mohit Muhal <mohitmuhal15122003@gmail.com>
2026-01-04 15:04:12 +0530

    initial commit

---
+++ dateDemo.java
@@ -0,0 +1,11 @@
+import java.time.LocalDate;
+import java.util.*;
+public class dateDemo {
+    public static void main(String[] args) {
+        Date d = new Date();
+        System.out.println(d);
+
+        LocalDate ld = LocalDate.now();
+        System.out.println(ld);
+    }
+}
