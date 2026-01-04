7501011b33e7f194bff5a1c25807b384c31f7962
Mohit Muhal <mohitmuhal15122003@gmail.com>
2026-01-04 17:10:59 +0530

    initial commit

--- dateDemo.java
+++ dateDemo.java
@@ -1,11 +1,14 @@
+import java.time.Clock;
 import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.*;
 public class dateDemo {
     public static void main(String[] args) {
         Date d = new Date();
         System.out.println(d);
 
-        LocalDate ld = LocalDate.now();
-        System.out.println(ld);
+        LocalDate ld = LocalDate.now(ZoneId.of("Asia/Kolkata"));
+        LocalDate ld1 = LocalDate.of(2067, 11, 15);
+        System.out.println(ld1);
     }
 }
