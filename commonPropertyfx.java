523f3a654daac6c710bcb0c2b4fd953d22d8cce0
Mohit Muhal <mohitmuhal15122003@gmail.com>
2026-01-11 17:38:58 +0530

    initial commit

--- commonPropertyfx.java
+++ commonPropertyfx.java
@@ -1,14 +1,25 @@
 import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Cursor;
 import javafx.scene.Scene;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.Border;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.paint.Color;
 import javafx.stage.*;
 import javafx.scene.control.*;
+
+
 public class commonPropertyfx extends Application {
 
     public void start(Stage stage){
         Button b = new Button("Click-me");
-        b.setTextFill(Color.PINK);
+        b.setTextFill(Color.BROWN);
+        b.setBackground(Background.fill(Color.PINK));
+        b.setCursor(Cursor.CLOSED_HAND);
+        b.setStyle("-fx-border-color-yellow;");
+        Alert a = new Alert(Alert.AlertType.CONFIRMATION,"Button is clicked");
+        b.setOnAction(e->a.show());
         FlowPane fp = new FlowPane(b);
         Scene sc = new Scene(fp,400,400);
         stage.setScene(sc);
