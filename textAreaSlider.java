a1d826deb82a78636719ad45fce86eafd45278ca
Mohit Muhal <mohitmuhal15122003@gmail.com>
2026-01-11 21:16:44 +0530

    initial commit

--- textAreaSlider.java
+++ textAreaSlider.java
@@ -1,7 +1,10 @@
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.scene.control.Slider;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 import javafx.stage.*;
 import javafx.scene.*;
 public class textAreaSlider extends Application
@@ -10,10 +13,17 @@ public class textAreaSlider extends Application
     Slider sl;
     public void start(Stage stage)
     {
-        ta = new TextArea("20");
-        sl = new Slider();
-        FlowPane fp = new FlowPane(ta);
-        Scene sc = new Scene(fp);
+        ta = new TextArea();
+        ta.setPrefColumnCount(100);
+        ta.setPrefRowCount(50);
+        sl = new Slider(10,100,10);
+        VBox vb = new VBox();
+        sl.setMajorTickUnit(5);
+        sl.setShowTickLabels(true);
+        sl.setShowTickMarks(true);
+        sl.valueProperty().addListener(e->ta.setFont(Font.font(sl.getValue())));
+        vb.getChildren().addAll(ta,sl);
+        Scene sc = new Scene(vb,500,500);
         stage.setScene(sc);
         stage.show();
     }
