19666ac6ade9838644446ef15ce400b73f5d3432
Mohit Muhal <mohitmuhal15122003@gmail.com>
2026-01-11 17:58:20 +0530

    initial commit

--- eventFX.java
+++ eventFX.java
@@ -1,18 +1,28 @@
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.FlowPane;
 import javafx.stage.*;
-public class eventFX extends Application{
-
+public class eventFX extends Application implements EventHandler<ActionEvent>
+{
+    int count = 0;
+    Button b;
     public void start(Stage stage){
-        Button b = new Button("click me");
+        b = new Button("click me");
+        b.setOnAction(this);
         b.setPrefSize(100.0,70.0);
         FlowPane fp = new FlowPane(b);
         Scene sc = new Scene(fp);
         stage.setTitle("Event Handling");
         stage.setScene(sc);
         stage.show();
+    }
+
+    public void handle(ActionEvent e){
+        count++;
+        b.setText(" "+count);
     }
     public static void main() {
     launch();
